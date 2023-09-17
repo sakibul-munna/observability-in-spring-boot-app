3392767dc1f485a4a155fbb2f4b6b098cd0aa56c
Sakibul_Munna <sakibul.munnaa@gmail.com>
2023-09-17 16:41:39 +0600

    A bash script to run the application on opentelemtry agent.

--- run-microservice.sh
+++ run-microservice.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 mvn clean package -Dmaven.test.skip=true
+AGENT_FILE=opentelemetry-javaagent-all.jar
 
-java -jar target/hello-app-1.0.jar
+if [ ! -f "${AGENT_FILE}" ]; then
+  curl -L https://github.com/aws-observability/aws-otel-java-instrumentation/releases/download/v1.28.1/aws-opentelemetry-agent.jar --output ${AGENT_FILE}
+fi
+
+export OTEL_TRACES_EXPORTER=otlp
+export OTEL_METRICS_EXPORTER=otlp
+export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:5555
+export OTEL_RESOURCE_ATTRIBUTES=service.name=hello-app,service.version=1.0
+
+java -javaagent:./${AGENT_FILE} -jar target/hello-app-1.0.jar
